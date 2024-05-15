4d338ff1db13809e238153942b0e355e01e8702b
bbabbi <mooncheayoung@gmail.com>
2024-05-15 16:06:35 +0900

    #4 ✨ feat : Book & Author 엔티티

--- src/main/java/com/brunch/server/author/entity/Author.java
+++ src/main/java/com/brunch/server/author/entity/Author.java
@@ -1,6 +1,9 @@
 package com.brunch.server.author.entity;
 
+import com.brunch.server.book.entity.Book;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class Author {
@@ -9,4 +12,16 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "author_id")
     private Long id;
+
+    @Column(name = "author_name")
+    private String name;
+
+    @Column(name = "author_description")
+    private String description;
+
+    private String job;
+    private Long subscriber;
+
+    @OneToMany(mappedBy = "author")
+    private List<Book> books;
 }
--- src/main/java/com/brunch/server/book/entity/Book.java
+++ src/main/java/com/brunch/server/book/entity/Book.java
@@ -1,5 +1,6 @@
 package com.brunch.server.book.entity;
 
+import com.brunch.server.author.entity.Author;
 import jakarta.persistence.*;
 
 @Entity
@@ -9,4 +10,22 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "book_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Author author;
+
+    private String title;
+
+    private int bookEpisode;
+
+    private int bookRuntime;
+
+    private String bookRecommendation;
+
+    private String bookDescription;
+
+    private String bookImage;
+
+    private int progress;
+
 }
