f9fdb56f507d3a3ce4543057e18bd01845ed0297
Chan531 <chan657312@gmail.com>
2024-05-23 14:15:21 +0900

    [CHORE] 불필요한 import문 제거

--- src/main/java/com/brunch/server/posting/repository/PostingRepository.java
+++ src/main/java/com/brunch/server/posting/repository/PostingRepository.java
@@ -2,7 +2,6 @@ package com.brunch.server.posting.repository;
 
 import com.brunch.server.posting.entity.Posting;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,5 +12,6 @@ public interface PostingRepository extends JpaRepository<Posting, Long> {
     List<Posting> findAllByIsLiked(int isLiked);
 
     List<Posting> findAllByDayOrderByRequiredTimeDesc(String day);
+
     List<Posting> findAllByDayOrderByLikeCountDesc(String day);
 }
